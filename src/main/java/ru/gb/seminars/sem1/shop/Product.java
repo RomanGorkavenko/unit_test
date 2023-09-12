8bdf2e9998d46918068bfe81fcc8ef9de5310498
RomanGorkavenko <roman@gorkavenko.ru>
2023-09-12 14:20:58 +0300

    added homework for sem1

---
+++ src/main/java/ru/gb/seminars/sem1/shop/Product.java
@@ -0,0 +1,31 @@
+package ru.gb.seminars.sem1.shop;
+
+public class Product {
+    private Integer cost; // Стоимость продукта
+    private String title; // Название
+
+    // Геттеры, сеттеры:
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "cost=" + cost +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/ru/gb/seminars/sem1/shop/SortProduct.java
@@ -0,0 +1,11 @@
+package ru.gb.seminars.sem1.shop;
+
+import java.util.Comparator;
+
+public class SortProduct implements Comparator<Product> {
+
+    @Override
+    public int compare(Product o1, Product o2) {
+        return o1.getCost() - o2.getCost();
+    }
+}
